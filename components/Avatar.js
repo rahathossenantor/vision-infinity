512b3a225002e91d807dd01d4dfeb64386e100c6
mdrahathossenantor <md.rahathossenantor@gmail.com>
2023-07-16 18:32:08 +0600

    home section completed

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,5 +1,10 @@
+// next image
+import Image from 'next/image'
+
 const Avatar = () => {
-  return <div>Avatar</div>;
+  return <div className='hidden xl:flex xl:max-w-none'>
+    <Image src={'/avatar.png'} width={697} height={638} alt='' className='translate-z-0 w-full h-full'/>
+  </div>;
 };
 
 export default Avatar;
--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -1,5 +1,21 @@
+// next image
+import Image from 'next/image';
+
+// next link
+ import Link from 'next/link';
+
+ // icons
+ import { HiArrowRight } from 'react-icons/hi2';
+
 const ProjectsBtn = () => {
-  return <div>Project Btn</div>;
+  return (
+    <div className='mx-auto xl:mx-0'>
+      <Link href={'/work'} className='relative w-[175px] h-[175px] flex justify-center items-center bg-circleStar bg-cover bg-center bg-no-repeat group:'>
+        <img src={'/rounded-text.png'} width={121} height={128} alt='' className='animate-spin-slow w-full h-full max-w-[121px] max-h-[128px]'/>
+        <HiArrowRight className='absolute text-4xl group-hover:translate-x-2 transition-all duration-300'/>
+      </Link>
+    </div>
+  );
 };
 
 export default ProjectsBtn;
